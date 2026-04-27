a815c0b2aeaf5b872e9a2c888a2e0cef4ff42ad3
maksym <gapachilomaxim@gmail.com>
2026-04-27 14:50:38 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import FishingTable from './components/FishingTable.vue'
+</script>
+
+<template>
+  <main>
+    <FishingTable />
+  </main>
+</template>
+
+<style scoped>
+main {
+  min-height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
